6a1c645f21ce04ad9b96d004c219f09bfe37dc7e
Thane <thabay0606@students.maquoketaschools.org>
2026-05-05 18:52:07 +0000

    Maybe..

--- Classroom.cs
+++ Classroom.cs
@@ -3,8 +3,8 @@
 public class Classroom{
 
     public List<string> CreateTeam(string first, string second, string third){
-        List<string> team = CreateTeam("first", "second", "third");
+        List<string> team = CreateTeam($"{first}", $"{second}", $"{third}");
         
-        return team;
+        return team; 
     }
 }
